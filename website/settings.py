79eb247986f595772d186e21736211d9dbcebe68
terran5992 <56596358+terran5992@users.noreply.github.com>
2020-01-31 12:05:31 +0800

    static files update

--- website/settings.py
+++ website/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
                 os.path.join(BASE_DIR,'static'), # if your static files folder is named "staticfiles"
