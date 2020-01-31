1907c7f3988b8a8a61a18f06781bea0bb2bb3348
terran5992 <56596358+terran5992@users.noreply.github.com>
2020-01-31 12:20:37 +0800

    static files update

--- website/wsgi.py
+++ website/wsgi.py
@@ -12,11 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # White Noise Settings
-from whitenoise.django import DjangoWhiteNoise
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-application.settings')
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# from whitenoise.django import DjangoWhiteNoise
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
